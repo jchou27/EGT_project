80a2291b914f82104d82da8a39a319135d824beb
Jack Chou <jack.h.chou.27@dartmouth.edu>
2026-05-17 20:55:30 -0400

    EGT update

--- notebooks/_runall.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call C:\Users\sharp\miniforge3\Scripts\activate.bat qss20
-cd /d C:\Users\sharp\Documents\GitHub\EGT_project\notebooks
-python -m jupyter nbconvert --to notebook --execute --inplace %1
